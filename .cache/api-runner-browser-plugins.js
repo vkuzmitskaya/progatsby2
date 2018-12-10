670e23577f2e1516693d2506a85ae642c89f629f
Viktoryia Kuzmitskaya <vkuzmitskaya@humanseelabs.com>
2018-12-10 15:18:38 +0300

    removed cache

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/humanseelabs/Documents/Archive/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/humanseelabs/Documents/Archive/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
