e38316b09e5398d068c48c8df74e9e889de22d86
Viktoryia Kuzmitskaya <vkuzmitskaya@humanseelabs.com>
2018-12-11 15:10:05 +0300

    Added netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,40 +2,43 @@ module.exports = {
   siteMetadata: {
     title: 'Level Up Blog',
     description: 'A new blog!',
+    siteUrl: 'https://sharp-jang-e7f473.netlify.com',
   },
   plugins: [
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Level Up Blog',
+        short_name: 'LUTBlog',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#524763',
+        theme_color: '#524763',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-image',
     'gatsby-plugin-offline',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/posts`,
-        name: 'posts'
-      }
+        name: 'posts',
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/images`,
-        name: 'images'
-      }
+        name: 'images',
+      },
     },
-    'gatsby-transformer-remark'
+    'gatsby-transformer-remark',
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify`, // make sure to put last in the array
   ],
 }
